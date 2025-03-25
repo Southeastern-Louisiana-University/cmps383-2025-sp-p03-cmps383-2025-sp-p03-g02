f36e4073f7e0497de4db68f746638b0a0defa97b
DylanGreen12 <98060477+DylanGreen12@users.noreply.github.com>
2025-03-25 00:46:44 -0500

    /testmenu route
    Menu table with a page to edit the table, adding works but updates/deletes not currently working, can view and image url works

--- Selu383.SP25.P03.Web/src/App.tsx
+++ Selu383.SP25.P03.Web/src/App.tsx
@@ -1,5 +1,4 @@
-//import { useState } from 'react'
-import './App.css'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Movies from "./pages/Movies.tsx";
 import Theaters from "./pages/Theaters.tsx";
@@ -7,6 +6,7 @@ import Tickets from "./pages/Tickets.tsx";
 import Food from "./pages/Food.tsx";
 import Account from "./pages/Account.tsx";
 import Navbar from "./components/Navbar.tsx";
+import { AddFoodItemForm } from './pages/TestFoodMenu.tsx'; // Import the form
 
 function App() {
     return (
@@ -18,12 +18,14 @@ function App() {
                 <Route path="/tickets" element={<Tickets />} />
                 <Route path="/food" element={<Food />} />
                 <Route path="/account" element={<Account />} />
+                <Route path="/testmenu" element={<AddFoodItemForm onAddFoodSuccess={() => {}} />} /> {/* Add route for the form */}
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
 
 
 
