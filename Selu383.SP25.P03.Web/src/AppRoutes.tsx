e1718295fe413477ed5e4c82cbbae1997e2dc08b
DylanGreen12 <98060477+DylanGreen12@users.noreply.github.com>
2025-03-25 20:36:16 -0500

    Route fix

---
+++ Selu383.SP25.P03.Web/src/AppRoutes.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Movies from "./pages/Movies";
+import Theaters from "./pages/Theaters";
+import Tickets from "./pages/Tickets";
+import Food from "./pages/Food";
+import Account from "./pages/Account";
+import Management from "./pages/Management.tsx";
+import { AddFoodItemForm } from './pages/EditFoodMenu.tsx';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/theaters" element={<Theaters />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/food" element={<Food />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/management" element={<Management />} /> {/* Added Management route */}
+            <Route path="/editmenu" element={<AddFoodItemForm onAddFoodSuccess={() => {}} />} /> {/* Add route for the form */}
+        </Routes>
+    );
+};
+
+export default AppRoutes;
