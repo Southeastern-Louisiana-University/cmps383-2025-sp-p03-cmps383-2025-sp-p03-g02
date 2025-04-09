06566da2dcb67e7de550b91620e1daae13172d97
brad jarreau <brad.jarreau@selu.edu>
2025-04-09 09:37:03 -0500

    fix
    fix

--- Selu383.SP25.P03.Mobile/app/_layout.tsx
+++ Selu383.SP25.P03.Mobile/app/_layout.tsx
@@ -5,7 +5,6 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { StripeProvider } from '@stripe/stripe-react-native';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -32,18 +31,13 @@ export default function RootLayout() {
   }
 
   return (
-    <StripeProvider
-      publishableKey="pk_test_51R6iZJRwJn1j2YGNWhhJdK3GLcSFHgBLXh9cJnka0NDZspaazlb76yP6iY4AilQgwPdqdcC05uAi0HGamxCkIc7700R1UHtFRy"
-      merchantIdentifier="merchant.com.lionsdencinemas"
-    >
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="auth" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </StripeProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="auth" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
